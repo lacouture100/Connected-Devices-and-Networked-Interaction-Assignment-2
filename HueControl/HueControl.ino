885c74e9e4ae4a071985bd0b1bbef28c064c24ad
Alvaro Lacouture <lacouture100@gmail.com>
2020-02-16 14:17:43 -0500

    Working example with screen

--- HueControl/HueControl.ino
+++ HueControl/HueControl.ino
@@ -16,11 +16,20 @@
    modified 2 Feb 2020
    by Tom Igoe (tigoe)
 */
+#include <Wire.h>
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <ArduinoHttpClient.h>
 #include <Arduino_JSON.h>
 #include "arduino_secrets.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 // make a wifi instance and a HttpClient instance:
 WiFiClient wifi;
 HttpClient httpClient = HttpClient(wifi, SECRET_HUBADDR);
@@ -32,50 +41,45 @@ int hue = 0;
 int sensorPin = A7;    // select the input pin for the potentiometer     // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 int mappedValue = 0;
+
+
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial); // wait for serial port to connect.
-  // attempt to connect to Wifi network:
+    while (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println("SSD1306 setup...");
+    delay(100);
+  }
+  
   while ( WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(SECRET_SSID);
     // Connect to WPA/WPA2 network:
+    displayWrite("connecting:" + String(SECRET_SSID),0,0);
     WiFi.begin(SECRET_SSID, SECRET_PASS);
     delay(2000);
   }
+  displayWrite("connected",0,0);
   // you're connected now, so print out the data:
   Serial.print("You're connected to the network IP = ");
   IPAddress ip = WiFi.localIP();
   Serial.println(ip);
 }
 void loop() {
-  // fade light up and down
-    
+
+  
   sensorValue = analogRead(sensorPin);
   mappedValue = map(sensorValue,0,1023,0,60000);
   Serial.println(mappedValue);
   lightState["hue"] = mappedValue;
   
-  if (brightness >= 255 || brightness <= 0) {
-    difference = -difference;
-  }
-  brightness += difference;
-  // keep it in range regardless of the difference value:
-  brightness = constrain(brightness, 0, 255);
-  
-  // set the "bri" property:
-  lightState["bri"] = brightness;
- 
-  // set the "on" property:
-  if (brightness > 0) {
-    lightState["on"] = true;
-  } else {
-    lightState["on"] = false;
-  }
+
   sendRequest(6, lightState);   // turn light on
-  
-  delay(100);                  // wait 1 second
+    displayLightData("Light" + String(SECRET_HUBADDR),0,0,
+                 "Hue:" + String(mappedValue),0,10,
+                 "Bright:" + String(mappedValue),0,20);
+            
+
 }
 void sendRequest(int light, JSONVar myState) {
   // make a String for the HTTP request path:
@@ -101,4 +105,24 @@ void sendRequest(int light, JSONVar myState) {
   Serial.print("Server response: ");
   Serial.println(response);
   Serial.println();
+}
+void displayWrite(String message, int x, int y) {
+  display.clearDisplay();
+  display.setTextSize(1); // Draw 2X-scale text
+  display.setTextColor(WHITE);
+  display.setCursor(x, y);
+  display.println(message);
+  display.display();
+}
+void displayLightData(String message1, int x1, int y1,String message2, int x2, int y2,String message3, int x3, int y3) {
+  display.clearDisplay();
+  display.setTextSize(1); // Draw 2X-scale text
+  display.setTextColor(WHITE);
+  display.setCursor(x1, y1);
+  display.println(message1);
+    display.setCursor(x2, y2);
+  display.println(message2);
+    display.setCursor(x3, y3);
+  display.println(message3);
+  display.display();
 }
